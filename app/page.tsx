cac4d01bb1248bda1326480f64cb4574dd6c25e1
Shubham Patel <shubham26062002@gmail.com>
2024-03-11 18:22:42 +0530

    Added card wrapper component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Poppins } from 'next/font/google'
 
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
-import { LoginButton } from '@/components/ui/auth/login-button'
+import { LoginButton } from '@/components/auth/login-button'
 
 const font = Poppins({
   subsets: ['latin'],
